023215cdf97d29812ed2bd829e41aa9690410c87
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-06-06 11:29:13 +0545

    10.18 Completed
    <Formik> use garera garda pani oostai same garnai parne raicha
    props pass garera garna parne
    meaning euta mockFormik banaunai parne
    tesaile try garera ignore garidiyo
    puranai ma return back

--- rate-repository-app/src/__tests__/components/SignInForm.js
+++ rate-repository-app/src/__tests__/components/SignInForm.js
@@ -6,32 +6,36 @@ import {
 } from "@testing-library/react-native";
 import { SignInForm } from "../../components/SignIn";
 
-describe("SigninForm", () => {
-  it("calls onSubmit with correct arguments when form submitted", async () => {
-    const onSubmitMock = jest.fn();
-    const formikMock = {
-      values: { username: "", password: "" },
-      handleChange: jest.fn(field => (value) => { formikMock.values[field] = value; }),
-      handleSubmit: jest.fn(() => {
-        onSubmitMock(formikMock.values);
-      }),
-      touched: { username: true, password: true },
-      errors: {},
-    };
+describe("Signin", () => {
+  describe("SigninForm", () => {
+    it("calls submit function with correct arguments when form submitted", async () => {
+      const onSubmitMock = jest.fn();
+      const formikMock = {
+        values: { username: "", password: "" },
+        handleChange: jest.fn((field) => (value) => {
+          formikMock.values[field] = value;
+        }),
+        handleSubmit: jest.fn(async () => {
+          await onSubmitMock(formikMock.values);
+        }),
+        touched: { username: true, password: true },
+        errors: {},
+      };
 
-    render(<SignInForm formik={formikMock} />);
-    const usernameInput = screen.getByPlaceholderText("Username");
-    const passwordInput = screen.getByPlaceholderText("Password");
-    const submitButton = screen.getByText("Sign in");
+      render(<SignInForm formik={formikMock} />);
+      const usernameInput = screen.getByPlaceholderText("Username");
+      const passwordInput = screen.getByPlaceholderText("Password");
+      const submitButton = screen.getByText("Sign in");
 
-    fireEvent.changeText(usernameInput, "testUser");
-    fireEvent.changeText(passwordInput, "testPass");
-    fireEvent.press(submitButton);
+      fireEvent.changeText(usernameInput, "testUser");
+      fireEvent.changeText(passwordInput, "testPass");
+      fireEvent.press(submitButton);
 
-    await waitFor(() => {
-      expect(onSubmitMock).toHaveBeenCalledWith({
-        username: "testUser",
-        password: "testPass",
+      await waitFor(() => {
+        expect(onSubmitMock).toHaveBeenCalledWith({
+          username: "testUser",
+          password: "testPass",
+        });
       });
     });
   });
